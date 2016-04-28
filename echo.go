c945d0e7df3ea0d2f996743fd9a2aba88d6492d9
Tomato Ketchup <r@554.jp>
2016-04-28 20:29:45 +0900

    Echo にズンドコキヨシ追加

--- echo.go
+++ echo.go
@@ -1,8 +1,12 @@
 package bot
 
 import (
+	"bytes"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/kechako/line-bot-client"
 	"github.com/pkg/errors"
@@ -13,6 +17,17 @@ import (
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
+)
+
+const (
+	zun     = "ズン"
+	doko    = "ドコ"
+	kiyoshi = "キ・ヨ・シ！"
+)
+
+var (
+	keywords = []string{"zundoko", "ズンドコ", "ずんどこ"}
+	random   = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +51,12 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = sendEcho([]string{msg.From}, msg.Text, ctx)
+	text := msg.Text
+	if isZundoko(text) {
+		text = zundoko()
+	}
+
+	err = sendEcho([]string{msg.From}, text, ctx)
 	if err != nil {
 		log.Errorf(ctx, "%v\n", err)
 		http.Error(w, "Send echo error.", http.StatusInternalServerError)
@@ -63,4 +83,38 @@ func sendEcho(to []string, text string, ctx context.Context) error {
 	log.Infof(ctx, "Send event sucess : %s\n", body)
 
 	return nil
+}
+
+func isZundoko(text string) bool {
+	for _, word := range keywords {
+		if strings.Contains(text, word) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func zundoko() string {
+	zundoko := [2]string{zun, doko}
+	good := [5]string{zun, zun, zun, zun, doko}
+
+	var current [5]string
+
+	reply := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	for current != good {
+		shift(&current)
+		zd := zundoko[random.Intn(2)]
+		current[4] = zd
+		reply.WriteString(zd)
+	}
+
+	reply.WriteString(kiyoshi)
+
+	return reply.String()
+}
+
+func shift(a *[5]string) {
+	a[0], a[1], a[2], a[3], a[4] = a[1], a[2], a[3], a[4], ""
 }
